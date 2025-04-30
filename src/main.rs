209b5d3e803a29de73b4410b5803eb593d112450
Valentine Briese <valentinegb@icloud.com>
2025-04-29 22:24:49 -0700

    Support transparency*
    *Pixels with 0 alpha are invisible, all others are opaque.

--- src/main.rs
+++ src/main.rs
@@ -14,42 +14,68 @@ fn main() {
     let img_height = img.height();
 
     for mut y in 0..img_height.div_ceil(2) {
-        const ESCAPE: char = '\x1B';
-        const CONTROL_SEQUENCE_INTRODUCER: char = '[';
-        const SELECT_GRAPHIC_RENDITION: char = 'm';
-        const RESET: &str = "0";
-
         y *= 2;
 
         for x in 0..img.width() {
+            const ESCAPE: char = '\x1B';
+            const CONTROL_SEQUENCE_INTRODUCER: char = '[';
+            const SELECT_GRAPHIC_RENDITION: char = 'm';
+            const RESET: &str = "0";
             const SET_FOREGROUND_COLOR: &str = "38";
             const SET_BACKGROUND_COLOR: &str = "48";
             const TWENTY_FOUR_BIT: &str = "2";
 
-            let top_pixel = img.get_pixel(x, y).to_rgb();
+            let top_pixel = img.get_pixel(x, y);
             let top_channels = top_pixel.channels();
-            let (top_r, top_g, top_b) = (top_channels[0], top_channels[1], top_channels[2]);
-
-            print!(
-                "{ESCAPE}{CONTROL_SEQUENCE_INTRODUCER}{SET_FOREGROUND_COLOR};{TWENTY_FOUR_BIT};{top_r};{top_g};{top_b}{SELECT_GRAPHIC_RENDITION}",
+            let (top_r, top_g, top_b, top_a) = (
+                top_channels[0],
+                top_channels[1],
+                top_channels[2],
+                top_channels[3],
             );
-
+            let top_rendered = top_a != 0;
             let bottom_y = y + 1;
-
-            if bottom_y < img_height {
-                let bottom_pixel = img.get_pixel(x, y + 1).to_rgb();
+            let bottom_channels = (bottom_y < img_height).then(|| {
+                let bottom_pixel = img.get_pixel(x, y + 1);
                 let bottom_channels = bottom_pixel.channels();
-                let (bottom_r, bottom_g, bottom_b) =
-                    (bottom_channels[0], bottom_channels[1], bottom_channels[2]);
+
+                (
+                    bottom_channels[0],
+                    bottom_channels[1],
+                    bottom_channels[2],
+                    bottom_channels[3],
+                )
+            });
+            let bottom_rendered = bottom_channels
+                .is_some_and(|(_bottom_r, _bottom_g, _bottom_b, bottom_a)| bottom_a != 0);
+
+            if top_rendered {
+                print!(
+                    "{ESCAPE}{CONTROL_SEQUENCE_INTRODUCER}{SET_FOREGROUND_COLOR};{TWENTY_FOUR_BIT};{top_r};{top_g};{top_b}{SELECT_GRAPHIC_RENDITION}",
+                );
+
+                if bottom_rendered {
+                    let (bottom_r, bottom_g, bottom_b, _bottom_a) = bottom_channels.unwrap();
+
+                    print!(
+                        "{ESCAPE}{CONTROL_SEQUENCE_INTRODUCER}{SET_BACKGROUND_COLOR};{TWENTY_FOUR_BIT};{bottom_r};{bottom_g};{bottom_b}{SELECT_GRAPHIC_RENDITION}",
+                    );
+                }
+
+                print!("▀");
+            } else if bottom_rendered {
+                let (bottom_r, bottom_g, bottom_b, _bottom_a) = bottom_channels.unwrap();
 
                 print!(
-                    "{ESCAPE}{CONTROL_SEQUENCE_INTRODUCER}{SET_BACKGROUND_COLOR};{TWENTY_FOUR_BIT};{bottom_r};{bottom_g};{bottom_b}{SELECT_GRAPHIC_RENDITION}",
+                    "{ESCAPE}{CONTROL_SEQUENCE_INTRODUCER}{SET_FOREGROUND_COLOR};{TWENTY_FOUR_BIT};{bottom_r};{bottom_g};{bottom_b}{SELECT_GRAPHIC_RENDITION}▄",
                 );
+            } else {
+                print!(" ");
             }
 
-            print!("▀");
+            print!("{ESCAPE}{CONTROL_SEQUENCE_INTRODUCER}{RESET}{SELECT_GRAPHIC_RENDITION}");
         }
 
-        println!("{ESCAPE}{CONTROL_SEQUENCE_INTRODUCER}{RESET}{SELECT_GRAPHIC_RENDITION}");
+        println!();
     }
 }
